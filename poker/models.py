81659950c8287f3f4b8f7b7e7a0b6632be23b6e3
farodey <farodey@yandex.ru>
2021-12-26 15:37:45 +0300

    добавил новые поля в модель Player

--- poker/models.py
+++ poker/models.py
@@ -10,18 +10,21 @@ class Player(models.Model):
     name = models.CharField(max_length=255)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     chips = models.IntegerField()
-    game_position = models.IntegerField()
+    bb = models.BooleanField()
+    sb = models.BooleanField()
     sitting_out = models.BooleanField()
+    active = models.BooleanField()
+    empty = models.BooleanField()
 
 
 class Hand(models.Model):
-    player = models.ManyToManyField(Player)
+    player = models.OneToOneField(Player, on_delete=models.CASCADE)
     card1 = models.CharField(max_length=5)
     card2 = models.CharField(max_length=5)
 
 
 class Board(models.Model):
-    game = models.ManyToManyField(Game)
+    game = models.OneToOneField(Game, on_delete=models.CASCADE)
     flop1 = models.CharField(max_length=5)
     flop2 = models.CharField(max_length=5)
     flop3 = models.CharField(max_length=5)
